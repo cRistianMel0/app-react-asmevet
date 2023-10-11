5f9a3336a8a5dec92d6b0b4a9c4923301fadfe33
“JuanSeGA” <117958247+JuanSeGArtor@users.noreply.github.com>
2023-10-11 03:31:06 -0500

    modificar modal ServicioCreate para que se recarge al crear un servicio

--- asmevet/src/pages/servicios/ServiciosCreate.jsx
+++ asmevet/src/pages/servicios/ServiciosCreate.jsx
@@ -6,14 +6,20 @@ import { Plus } from 'react-bootstrap-icons';
 import axios from 'axios';
 
 export default function ServiciosCreate() {
+  // Estado para controlar la visibilidad del modal
   const [show, setShow] = useState(false);
+
+  // Estado para almacenar los valores del formulario
   const [formValues, setFormValues] = useState({
     nombre: '',
     descripcion: '',
     imagen: '',
   });
+
+  // Estado para manejar la validación del formulario
   const [validated, setValidated] = useState(false);
 
+  // Función para cerrar el modal, limpiar el formulario y recargar la page
   const handleClose = () => {
     setShow(false);
     setFormValues({
@@ -22,18 +28,25 @@ export default function ServiciosCreate() {
       imagen: '',
     });
     setValidated(false);
+    window.location.reload();
   };
 
+  // Función para mostrar el modal
   const handleShow = () => setShow(true);
 
+  // Función para manejar el envío del formulario
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     const form = e.currentTarget;
+    
+    // Validar el formulario
     if (form.checkValidity() === false) {
       e.stopPropagation();
     } else {
       try {
+        // Realizar una solicitud POST a la API con los valores del formulario
         await axios.post("http://localhost:8000/servicios", formValues);
+        // Cerrar el modal después de enviar los datos
         handleClose();
       } catch (err) {
         console.log(err);
@@ -42,6 +55,7 @@ export default function ServiciosCreate() {
     setValidated(true);
   };
 
+  // Función para manejar los cambios en los campos del formulario
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormValues({
@@ -91,7 +105,7 @@ export default function ServiciosCreate() {
             <Form.Group className="mb-3">
               <Form.Label>Imagen</Form.Label>
               <Form.Control
-                type="file"
+                type="file" // Cambiado a type "file" para cargar imágenes
                 name="imagen"
                 value={formValues.imagen}
                 onChange={handleInputChange}
