750d5ba5930230e74f5793282f409561793a2a68
CristianMM <118846884+cRistianMel0@users.noreply.github.com>
2023-10-18 17:03:45 -0500

    agregar funcionalidad de deshabilitar servicios

--- asmevet/src/pages/servicios/components/CardServicios.jsx
+++ asmevet/src/pages/servicios/components/CardServicios.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
 import { PencilSquare, Trash } from 'react-bootstrap-icons';
 import "../styled-components/cardServicios.scss";
 import servicioImg from '../../../assets/img/servicio.jpeg';
+import axios from 'axios';
 
 export default function CardServicios({ cards }) {
   const handleEdit = (servicio) => {
-
+      console.log(servicio)
   }
 
-  const handleDelete = (servicio) => {
-    let res = confirm("¿Está seguro de que desea ELIMINAR el servicio?");
+  const handleDelete = (idServicio) => {
+    let res = window.confirm("¿Está seguro de que desea DESHABILITAR el servicio?");
+    if (res) {
+      axios.patch('http://localhost:8000/servicios', {idServicio})
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+          // Manejar el error si es necesario
+          window.alert(`Se ha generado un problema en el servidor ${error}`)
+        });
+    }
   }
+  
 
   return (
     <>
@@ -26,7 +39,7 @@ export default function CardServicios({ cards }) {
               <button className="edit-button" onClick={() => handleEdit(servicio)}>
                 <PencilSquare />
               </button>
-              <button className="delete-button" onClick={() => handleDelete(servicio)}>
+              <button className="delete-button"  onClick={() => handleDelete(servicio.idServicio)}>
                 <Trash />
               </button>
             </div>
