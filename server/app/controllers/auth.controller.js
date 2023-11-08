e5af5866cd9e73cec4f4ff18965d70d8852cddb6
cRistianMel0 <118846884+cRistianMel0@users.noreply.github.com>
2023-11-08 13:34:47 -0500

    merge manual del auth.controller.js

--- server/app/controllers/auth.controller.js
+++ server/app/controllers/auth.controller.js
@@ -1,11 +1,7 @@
 const db = require("../models");
-<<<<<<< HEAD
 const config = require("../config/auth.config");
 const User = db.user;
 const Role = db.role;
-=======
-const User = db.user;
->>>>>>> d494e8afdc3ac8d4809fed73bae95ee40a896cb9
 
 const Op = db.Sequelize.Op;
 
@@ -15,29 +11,16 @@ var bcrypt = require("bcryptjs");
 exports.signup = (req, res) => {
   // Save User to Database
   User.create({
-<<<<<<< HEAD
     nombre: req.body.nombre,
     apellido: req.body.apellido,
     tipoDoc: req.body.tipoDoc,
     documento: req.body.documento,
     telefono: req.body.telefono,
     direccion: req.body.direccion,
-=======
-    username: req.body.username,
-    apellido: req.body.apellido,
-    tipoDoc: req.body.tipoDoc || 'Cédula',
-    documento: req.body.documento,
-    telefono: req.body.telefono,
-    direccion: req.body.direccion,
-    correo: req.body.correo,
-    genero: req.body.genero,
-    fechaNacimiento: req.body.fechaNacimiento,
->>>>>>> d494e8afdc3ac8d4809fed73bae95ee40a896cb9
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 8)
   })
     .then(user => {
-<<<<<<< HEAD
       if (req.body.roles) {
         Role.findAll({
           where: {
@@ -56,9 +39,6 @@ exports.signup = (req, res) => {
           res.send({ message: "User registered successfully!" });
         });
       }
-=======
-      res.send({ message: "User registered successfully!" });
->>>>>>> d494e8afdc3ac8d4809fed73bae95ee40a896cb9
     })
     .catch(err => {
       res.status(500).send({ message: err.message });
@@ -68,11 +48,7 @@ exports.signup = (req, res) => {
 exports.signin = (req, res) => {
   User.findOne({
     where: {
-<<<<<<< HEAD
       email: req.body.email
-=======
-      username: req.body.username
->>>>>>> d494e8afdc3ac8d4809fed73bae95ee40a896cb9
     }
   })
     .then(user => {
@@ -100,7 +76,6 @@ exports.signin = (req, res) => {
                                 expiresIn: 86400, // 24 hours
                               });
 
-<<<<<<< HEAD
       var authorities = [];
       user.getRoles().then(roles => {
         for (let i = 0; i < roles.length; i++) {
@@ -112,22 +87,6 @@ exports.signin = (req, res) => {
           roles: authorities,
           accessToken: token
         });
-=======
-      res.status(200).send({
-        id: user.id,
-        username: user.username,
-        apellido: user.apellido,
-        tipoDoc: user.tipoDoc,
-        documento: user.documento,
-        telefono: user.telefono,
-        direccion: user.direccion,
-        correo: user.correo,
-        genero: user.genero,
-        fechaNacimiento: user.fechaNacimiento,
-        email: user.email,
-        roles: [], // No estoy seguro de cómo manejar los roles en tu aplicación, aquí los dejé vacíos
-        accessToken: token
->>>>>>> d494e8afdc3ac8d4809fed73bae95ee40a896cb9
       });
     })
     .catch(err => {
