a6798dd8c8f2380ef6a4a7c5d1080018283e73ba
“JuanSeGA” <117958247+JuanSeGArtor@users.noreply.github.com>
2023-11-22 09:21:32 -0500

    agregar componente SliderSection a Home
    -en Home se añade SliderSection.
    
    -se añaden imagenes para cada slide de SliderSection.
    
    -se crea un archivo de estilos para SliderSection, el cual es
    sliderSection.scss.

--- asmevet/src/pages/home/Home.jsx
+++ asmevet/src/pages/home/Home.jsx
@@ -1,13 +1,15 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import SliderSection from "./components/SliderSection";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <section>
-        <div style={{ height: "800px" }}>
-            
+      <section className="pt-5">
+        <div className="pt-5" style={{ height: "800px", backgroundColor: "white" }}>
+            <SliderSection />
+
         </div>
       </section>
       <Footer />
