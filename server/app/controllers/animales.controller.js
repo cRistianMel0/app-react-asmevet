d2777dd898d3d839a59799c4d0f67a0b68c740e1
cRistianMel0 <118846884+cRistianMel0@users.noreply.github.com>
2023-12-05 02:32:35 -0500

    agregar edit de una mascota

--- server/app/controllers/animales.controller.js
+++ server/app/controllers/animales.controller.js
@@ -22,3 +22,36 @@ exports.create = (req, res) => {
         });
       });
 }
+
+//funcion para editar la informacion de un animal
+exports.edit = (req, res) => {
+  const animalId = req.params.idAnimal; // Suponiendo que el ID del animal está en los parámetros de la URL
+
+  const updatedAnimal = {
+      idUser: req.body.idUser,
+      nombre: req.body.nombre,
+      tipo: req.body.tipo,
+      raza: req.body.raza,
+      collar: req.body.collar,
+      requiereCarnet: req.body.requiereCarnet,
+      fechaNacimiento: req.body.fechaNacimiento
+  }
+
+  Animal.findByPk(animalId) // Busca el animal por su ID
+    .then(animal => {
+      if (!animal) {
+        return res.status(404).send({ message: "Animal no encontrado." });
+      }
+
+      // Actualiza los campos del animal con los nuevos valores
+      return animal.update(updatedAnimal);
+    })
+    .then(updatedData => {
+      res.send(updatedData); // Envía los datos actualizados del animal
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Ocurrió un error al actualizar el Animal."
+      });
+    });
+}
